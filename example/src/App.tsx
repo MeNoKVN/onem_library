aac8c524589806ba64af43cb21d268a9be3198df
MeNoKVN <106679792+MeNoKVN@users.noreply.github.com>
2024-06-07 12:59:26 +0200

    chore: release 0.1.2 feat: implemented animated dropdown component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,10 +2,20 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { AnimatedDropdown } from 'react-native-onem_library';
 
+const options = [
+  { label: 'Option 1', value: 'option1' },
+  { label: 'Option 2', value: 'option2' },
+  { label: 'Option 3', value: 'option3' },
+];
+
+const handleSelect = (selectedValue: string) => {
+  console.log('Selected value:', selectedValue);
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <AnimatedDropdown />
+      <AnimatedDropdown options={options} onSelect={handleSelect} />
     </View>
   );
 }
@@ -13,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5F5F5',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/components/AnimatedDropdown/AnimatedDropdown.tsx
+++ src/components/AnimatedDropdown/AnimatedDropdown.tsx
@@ -1,14 +1,150 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import IIcon from 'react-native-vector-icons/Ionicons';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  FadeIn,
+  FadeInLeft,
+} from 'react-native-reanimated';
 
-interface AnimatedDropdownProps {}
+interface DropdownOption {
+  label: string;
+  value: string;
+}
 
-const AnimatedDropdown: React.FC<AnimatedDropdownProps> = () => {
+interface AnimatedDropdownProps {
+  options: DropdownOption[];
+  onSelect: (selectedValue: string) => void;
+  boxHeight?: number;
+  boxWidth?: number;
+  iconColor?: string;
+  placeholder?: string;
+}
+
+const AnimatedDropdown: React.FC<AnimatedDropdownProps> = ({
+  options,
+  onSelect,
+  boxHeight = 50,
+  boxWidth = 200,
+  iconColor = '#666',
+  placeholder = 'Select an option',
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<DropdownOption | null>(
+    null
+  );
+  const iconRotation = useSharedValue(0);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    iconRotation.value = withTiming(isOpen ? 0 : 180, { duration: 200 });
+  };
+
+  const handleOptionSelect = (option: DropdownOption) => {
+    setSelectedOption(option);
+    onSelect(option.value);
+    setIsOpen(false);
+    iconRotation.value = withTiming(0, { duration: 300 });
+  };
+
+  const iconStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${iconRotation.value}deg` }],
+    };
+  });
+
   return (
     <View>
-      <Text>AnimatedDropdown</Text>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={toggleDropdown}
+        style={[styles.selectBox, { height: boxHeight, width: boxWidth }]}
+      >
+        <Text style={styles.selectedOptionText}>
+          {selectedOption ? selectedOption.label : placeholder}
+        </Text>
+        <Animated.View style={iconStyle}>
+          <IIcon name="chevron-down" size={20} color={iconColor} />
+        </Animated.View>
+      </TouchableOpacity>
+      {isOpen && (
+        <Animated.View
+          style={[styles.dropdownContainer, { width: boxWidth }]}
+          entering={FadeIn}
+        >
+          {options.map((option, index) => (
+            <Animated.View
+              entering={FadeInLeft.duration(300)
+                .springify()
+                .damping(18)
+                .stiffness(200)}
+              key={option.value}
+            >
+              <TouchableOpacity
+                key={option.value}
+                style={[
+                  styles.optionItem,
+                  index !== options.length - 1 && styles.optionItemSeparator,
+                ]}
+                onPress={() => handleOptionSelect(option)}
+              >
+                <Text style={styles.optionText}>{option.label}</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          ))}
+        </Animated.View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  selectBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#eee',
+    borderRadius: 8,
+  },
+  selectedOptionText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  dropdownContainer: {
+    position: 'absolute',
+    top: '100%',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#eee',
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginTop: 4,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  optionItem: {
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  optionItemSeparator: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  optionText: {
+    fontSize: 16,
+    color: '#333',
+  },
+});
 
 export default AnimatedDropdown;
